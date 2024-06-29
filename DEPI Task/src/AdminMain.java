fa298a36aa5ae3264d33b82d4eaa376c663e42aa
Farouk Etman <122695019+Farouk-Osman@users.noreply.github.com>
2024-06-29 17:42:09 +0300

    Update AdminMain.java
    Add logout option

--- DEPI Task/src/AdminMain.java
+++ DEPI Task/src/AdminMain.java
@@ -1,85 +1,31 @@
-import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        UserMain userMain = new UserMain();
+        AdminMain adminMain = new AdminMain();
 
-public class AdminMain {
-    private Menu menu = new Menu();
-    private Addition addition = new Addition();
-    private Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    public AdminMain() {
-
-        menu.addSandwich(new SandwichItem("Beef Burger", 80.0));
-        menu.addSandwich(new SandwichItem("Chicken Burger", 70.0));
-        menu.addSandwich(new SandwichItem("Cheese Burger", 75.0));
-
-        addition.addAddition(new AdditionItem("Ketchup", 5.0));
-        addition.addAddition(new AdditionItem("Mayonnaise", 10.0));
-        addition.addAddition(new AdditionItem("Tomato", 5.0));
-        addition.addAddition(new AdditionItem("Cheese", 15.0));
-    }
-
-    public void run() {
         while (true) {
-            showAdminMenu();
+            System.out.println("Welcome to Burger-Masr :-\nWhich type of login do you want to use?");
+            System.out.println("1- User");
+            System.out.println("2- Admin");
+            System.out.println("3- Exit");
             int option = scanner.nextInt();
+
             switch (option) {
                 case 1:
-                    viewMenu();
+                    userMain.run();
                     break;
                 case 2:
-                    viewAdditions();
+                    adminMain.run();
                     break;
                 case 3:
-                    addNewSandwich();
-                    break;
-                case 4:
-                    addNewAddition();
+                    System.exit(0);
                     break;
                 default:
                     System.out.println("Invalid option");
             }
         }
-    }
-
-    public void showAdminMenu() {
-        System.out.println("1- View Menu");
-        System.out.println("2- View Additions");
-        System.out.println("3- Add New Item to Menu");
-        System.out.println("4- Add New Addition");
-    }
-
-    public void viewMenu() {
-        System.out.println("Menu:");
-        for (SandwichItem sandwich : menu.getSandwiches()) {
-            System.out.println(sandwich.getId() + "- " + sandwich.getName() + " (" + sandwich.getPrice() + ")");
-        }
-    }
-
-    public void viewAdditions() {
-        System.out.println("Additions:");
-        for (AdditionItem additionItem : addition.getAdditions()) {
-            System.out.println(additionItem.getId() + "- " + additionItem.getName() + " (" + additionItem.getPrice() + ")");
-        }
-    }
-
-    public void addNewSandwich() {
-        System.out.println("New Sandwich:");
-        scanner.nextLine(); // Consume newline
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter price: ");
-        double price = scanner.nextDouble();
-        SandwichItem newSandwich = new SandwichItem(name, price);
-        menu.addSandwich(newSandwich);
-    }
-
-    public void addNewAddition() {
-        System.out.println("New Addition:");
-        scanner.nextLine(); // Consume newline
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter price: ");
-        double price = scanner.nextDouble();
-        AdditionItem newAddition = new AdditionItem(name, price);
-        addition.addAddition(newAddition);
     }
 }
